472005f708f3a3bac8b5871d4e585badc21cef2c
Abhiyanshu Anand <abhiyanshu1107+github@gmail.com>
2025-09-25 13:35:49 +0530

    Update __init__.py

--- backend/authentication/__init__.py
+++ backend/authentication/__init__.py
@@ -1,9 +1,37 @@
 # Authentication app for NUDRRS
 
-# Import services and views to make them available at the package level
+# Import services to make them available at the package level
 from .services import auth_mongodb_service
 from .mongodb_service import AuthMongoDBService
-from .views import RegisterView
+
+# Import views to make them available at the package level
+from .views import (
+    RegisterView,
+    login_view,
+    logout_view,
+    profile_view,
+    change_password,
+    password_reset_request,
+    password_reset_verify_otp,
+    password_reset_confirm,
+    OrganizationListView,
+    OrganizationDetailView,
+    upload_profile_image
+)
 
 # Make these available when importing from authentication
-__all__ = ['auth_mongodb_service', 'AuthMongoDBService', 'RegisterView']
+__all__ = [
+    'auth_mongodb_service',
+    'AuthMongoDBService',
+    'RegisterView',
+    'login_view',
+    'logout_view',
+    'profile_view',
+    'change_password',
+    'password_reset_request',
+    'password_reset_verify_otp',
+    'password_reset_confirm',
+    'OrganizationListView',
+    'OrganizationDetailView',
+    'upload_profile_image'
+]
