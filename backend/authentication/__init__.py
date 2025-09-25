88bf852ba7828daba96e1d3d84c996538f1e4431
Abhiyanshu Anand <abhiyanshu1107+github@gmail.com>
2025-09-25 11:15:09 +0530

    Update __init__.py

--- backend/authentication/__init__.py
+++ backend/authentication/__init__.py
@@ -1,1 +1,8 @@
 # Authentication app for NUDRRS
+
+# Import services to make them available at the package level
+from .services import auth_mongodb_service
+from .mongodb_service import AuthMongoDBService
+
+# Make these available when importing from authentication
+__all__ = ['auth_mongodb_service', 'AuthMongoDBService']
