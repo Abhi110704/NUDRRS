0ea5abd2978ec884b0070461df61633975feaa73
Abhiyanshu Anand <abhiyanshu1107+github@gmail.com>
2025-09-25 13:56:29 +0530

    Update __init__.py

--- backend/authentication/__init__.py
+++ backend/authentication/__init__.py
@@ -4,8 +4,34 @@
 from .services import auth_mongodb_service
 from .mongodb_service import AuthMongoDBService
 
-# Only expose the service-related items
+# Import views
+from .views import (
+    RegisterView,
+    login_view,
+    logout_view,
+    profile_view,
+    change_password,
+    password_reset_request,
+    password_reset_verify_otp,
+    password_reset_confirm,
+    OrganizationListView,
+    OrganizationDetailView,
+    upload_profile_image
+)
+
+# Make these available when importing from authentication
 __all__ = [
     'auth_mongodb_service',
-    'AuthMongoDBService'
+    'AuthMongoDBService',
+    'RegisterView',
+    'login_view',
+    'logout_view',
+    'profile_view',
+    'change_password',
+    'password_reset_request',
+    'password_reset_verify_otp',
+    'password_reset_confirm',
+    'OrganizationListView',
+    'OrganizationDetailView',
+    'upload_profile_image'
 ]
