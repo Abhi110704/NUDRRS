cda4e2c233531fdc77fd4eb89e042980f6105724
Abhiyanshu Anand <abhiyanshu1107+github@gmail.com>
2025-09-25 10:59:57 +0530

    Update services.py

--- backend/authentication/services.py
+++ backend/authentication/services.py
@@ -2,7 +2,7 @@
 Service instances for the authentication app.
 This file is used to avoid circular imports by centralizing service instances.
 """
-from authentication.mongodb_service import AuthMongoDBService
+from .mongodb_service import AuthMongoDBService
 
 # Create a single instance of the AuthMongoDBService
 auth_mongodb_service = AuthMongoDBService()
